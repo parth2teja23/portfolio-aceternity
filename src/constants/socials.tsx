054c6118013434f444711aa98d227e1591487c1f
parth2teja23 <parth2teja@gmail.com>
2025-08-12 11:15:36 +0530

    repo init

---
+++ src/constants/socials.tsx
@@ -0,0 +1,23 @@
+import {
+  IconBrandLinkedin,
+  IconBrandTwitter,
+  IconBrandYoutube,
+} from "@tabler/icons-react";
+
+export const socials = [
+  {
+    href: "https://twitter.com/parth2teja",
+    label: "Twitter",
+    icon: IconBrandTwitter,
+  },
+  {
+    href: "https://linkedin.com/in/parth2teja",
+    label: "LinkedIn",
+    icon: IconBrandLinkedin,
+  },
+  {
+    href: "https://youtube.com/parth2teja",
+    label: "YouTube",
+    icon: IconBrandYoutube,
+  },
+];
